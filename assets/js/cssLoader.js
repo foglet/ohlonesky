faf5e387e83b453b4c82f9cff202c5d2a0d186a7
Fogfish <foglet@users.noreply.github.com>
2025-05-30 23:15:03 -0700

    Create cssLoader.js

---
+++ assets/js/cssLoader.js
@@ -0,0 +1,12 @@
+(function loadCSSAssets() {
+  const depth = window.location.pathname.split('/').filter(Boolean).length;
+  const prefix = '../'.repeat(depth);
+  const stylesheets = ['assets/css/output.css', 'assets/css/hero.css'];
+
+  stylesheets.forEach(file => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `${prefix}${file}`;
+    document.head.appendChild(link);
+  });
+})();
