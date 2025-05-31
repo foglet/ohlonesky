7ad3b81308e7bc76c6fecb44fe2125375e934087
Fogfish <foglet@users.noreply.github.com>
2025-05-31 00:04:18 -0700

    Update cssLoader.js

--- assets/js/cssLoader.js
+++ assets/js/cssLoader.js
@@ -1,17 +1,13 @@
-(function loadCSSAssets() {
+// /assets/js/cssLoader.js
+document.addEventListener('DOMContentLoaded', () => {
   const depth = window.location.pathname.split('/').filter(Boolean).length;
   const prefix = '../'.repeat(depth);
   const version = `?v=${Date.now()}`;
 
-  const stylesheets = [
-    'assets/css/output.css',
-    'assets/css/hero.css'
-  ];
-
-  stylesheets.forEach((file) => {
+  ['assets/css/output.css', 'assets/css/hero.css'].forEach(file => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = `${prefix}${file}${version}`;
     document.head.appendChild(link);
   });
-})();
+});
